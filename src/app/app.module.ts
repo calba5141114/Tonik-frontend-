a844a10091278f4b9ab9a4d82e9ecc563d09c659
KhallilB <khallil.bailey@gmail.com>
2018-10-27 14:35:30 -0700

    welcome message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
+import { GomeWelcomeComponent } from './gome-welcome/gome-welcome.component';
+import { HomeWelcomeComponent } from './home-welcome/home-welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeCarouselComponent,
+    GomeWelcomeComponent,
+    HomeWelcomeComponent
   ],
   imports: [
     BrowserModule,
