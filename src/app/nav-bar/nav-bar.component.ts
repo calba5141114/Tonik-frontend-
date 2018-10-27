4b074f8798770c1260d7053b7db3143e8e3342a7
KhallilB <khallil.bailey@gmail.com>
2018-10-27 16:59:25 -0700

    basic home  page

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+    
   }
 
 }
