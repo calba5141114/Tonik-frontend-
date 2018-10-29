0d97c6fced6041de54a65038483ee3cf25557bee
KhallilB <khallil.bailey@gmail.com>
2018-10-28 18:26:53 -0700

    artists page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 import { HomeWelcomeComponent } from './home-welcome/home-welcome.component';
 import { HomeUpdatesComponent } from './home-updates/home-updates.component';
+import { ArtistsComponent } from './artists/artists.component';
 
+// const appRoutes: Routes = [
+//   { path: 'artists', component: }
+// ]
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeCarouselComponent,
     HomeWelcomeComponent,
-    HomeUpdatesComponent
+    HomeUpdatesComponent,
+    ArtistsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
