a19dcf6fdf9290789b8a58fb4a65c61fd1a75311
KhallilB <khallil.bailey@gmail.com>
2018-11-19 17:06:39 -0800

    general users can login and redirects them to their profile page

--- src/app/forms/general-login/general-login.component.ts
+++ src/app/forms/general-login/general-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -10,18 +10,22 @@ export class GeneralLoginComponent implements OnInit {
 
   generalLogInData = {}
 
-  constructor(private _auth: AuthService) { }
+  constructor(private _auth: AuthService, private _ngZone: NgZone) { }
 
   ngOnInit() {
   }
 
+  //logs in general user an redirects them to profile page
   logInGeneral() {
     console.log(this.generalLogInData)
     this._auth.logInGeneral(this.generalLogInData)
       .subscribe(
         Response => {
           console.log(Response)
-          localStorage.setItem('token', Response)
+          localStorage.setItem('token', Response),
+          this._ngZone.runOutsideAngular(() => {
+            window.location.href = '/general-profile'
+          });
         },
         err => console.log(err)
       )
